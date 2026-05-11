13b966b4da0a46beddf4ce16cb590b0fe165758e
Valerii <valeraivasik602@gmail.com>
2026-05-11 22:00:31 +0100

    f

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -135,5 +135,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
